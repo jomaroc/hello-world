49ee2b68050368a128c1c23afa0b4697d7074d6f
JomaroC <jomaroc@gmail.com>
2018-02-07 19:55:39 +0000

    prueba3

--- Prueba.java
+++ Prueba.java
@@ -2,7 +2,7 @@ public class Prueba
 {
 	public static void main(String[] arg)
 	{
-		System.out.println("Hola Mundo Omar");
+		System.out.println("Hola Mundo Omar Roser");
 		System.out.println("Aja");
 		System.out.println("Adios");
 		
